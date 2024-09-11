9e007beede81920b6b9f4fcc07bf5cb385d6889f
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-09-11 09:22:35 -0400

    Add project files.

---
+++ Topic - 3 Variables Programming Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+
+namespace Topic___3_Variables_Programming_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tiago
+            int num1, num2;
+            string word;
+            double price = 11;
+
+            word = "sum: ";
+            num1 = 9;
+            num2 = 10;
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(word + num1 + num2);
+            Console.WriteLine(word + (num1 + num2));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int cost = 11;
+            int weight = 2;
+            Console.WriteLine(cost / weight);
+            Console.WriteLine(price / weight);
+        }
+    }
+}
